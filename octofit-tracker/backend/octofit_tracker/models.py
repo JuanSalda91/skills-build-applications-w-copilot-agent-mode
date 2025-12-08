4718bd461501e6f611021a494bff4f4226d7b54e
Juan Saldarriaga <juansalda9131@gmail.com>
2025-12-08 04:03:29 +0000

    Fix: Add ObjectIdField as primary key to all models for valid id fields in API responses

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,6 +1,7 @@
 from djongo import models
 
 class User(models.Model):
+    id = models.ObjectIdField(primary_key=True, default=True)
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=100)
     team = models.CharField(max_length=50)
@@ -9,11 +10,13 @@ class User(models.Model):
         db_table = 'users'
 
 class Team(models.Model):
+    id = models.ObjectIdField(primary_key=True, default=True)
     name = models.CharField(max_length=50, unique=True)
     class Meta:
         db_table = 'teams'
 
 class Activity(models.Model):
+    id = models.ObjectIdField(primary_key=True, default=True)
     user = models.CharField(max_length=100)
     type = models.CharField(max_length=50)
     duration = models.IntegerField()
@@ -22,12 +25,14 @@ class Activity(models.Model):
         db_table = 'activities'
 
 class Leaderboard(models.Model):
+    id = models.ObjectIdField(primary_key=True, default=True)
     team = models.CharField(max_length=50)
     points = models.IntegerField()
     class Meta:
         db_table = 'leaderboard'
 
 class Workout(models.Model):
+    id = models.ObjectIdField(primary_key=True, default=True)
     name = models.CharField(max_length=100)
     difficulty = models.CharField(max_length=50)
     class Meta:
